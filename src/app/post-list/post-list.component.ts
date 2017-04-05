2a8c78b28714f5a1da8c2c3b707591bc1bd81895
Matt Martz <matt.martz@gmail.com>
2017-04-04 23:28:30 -0600

    UI tweaks and a new post
    + social links and forking

--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,5 +1,5 @@
-import {BlogPostService} from '../blog-post.service';
-import {BlogPost} from '../BlogPost';
+import { BlogPostService } from '../blog-post.service';
+import { BlogPost } from '../BlogPost';
 import { Component, OnInit, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 
@@ -12,7 +12,11 @@ export class PostListComponent implements OnInit {
   public posts: BlogPost[];
 
   constructor(private blogPostService: BlogPostService, private router: Router) {
-    this.posts = blogPostService.posts;
+    this.posts = blogPostService.posts.sort(function (a, b) {
+      const aDate = new Date(a.published);
+      const bDate = new Date(b.published);
+      return aDate > bDate ? -1 : aDate < bDate ? 1 : 0;
+    });
   }
 
   ngOnInit() {
